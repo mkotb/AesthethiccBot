a9390f85014c392920041c62c3dfa7edeab3ea6a
Mazen Kotb <mazenkotb@gmail.com>
2017-12-09 15:40:22 -0800

    Initial commit

---
+++ src/main/java/com/mkotb/aesthethicc/AesthethiccBot.java
@@ -0,0 +1,94 @@
+package com.mkotb.aesthethicc;
+
+import com.jtelegram.api.TelegramBot;
+import com.jtelegram.api.TelegramBotRegistry;
+import com.jtelegram.api.events.inline.InlineQueryEvent;
+import com.jtelegram.api.inline.input.InputTextMessageContent;
+import com.jtelegram.api.inline.result.InlineResultArticle;
+import com.jtelegram.api.requests.inline.AnswerInlineQuery;
+import com.jtelegram.api.requests.message.framework.ParseMode;
+import com.jtelegram.api.update.PollingUpdateProvider;
+import com.mkotb.aesthethicc.transform.ChillAesthethicc;
+import com.mkotb.aesthethicc.transform.ClapperAesthethicc;
+import com.mkotb.aesthethicc.transform.ClassicAesthethicc;
+import com.mkotb.aesthethicc.transform.YellAesthethicc;
+import lombok.Getter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+public class AesthethiccBot {
+    private TelegramBot bot;
+    private List<TextTransformer> transformers = new ArrayList<>();
+
+    private AesthethiccBot(String apiKey) {
+        transformers.add(new ClassicAesthethicc());
+        transformers.add(new ClapperAesthethicc());
+        transformers.add(new ChillAesthethicc());
+        transformers.add(new YellAesthethicc());
+
+        TelegramBotRegistry
+                .builder()
+                .updateProvider(new PollingUpdateProvider())
+                .build()
+                .registerBot(apiKey, (bot, error) -> {
+                    if (error != null) {
+                        System.out.println("Unable to start bot! Shutting down..");
+                        error.printStackTrace();
+
+                        try {
+                            Thread.sleep(1000L);
+                        } catch (InterruptedException ignored) {
+                            return;
+                        }
+
+                        System.exit(-1);
+                        return;
+                    }
+
+                    System.out.println("Logged in as @" + bot.getBotInfo().getUsername());
+                    this.bot = bot;
+
+                    bot.getEventRegistry().registerEvent(InlineQueryEvent.class, this::handleIncomingQuery);
+                });
+    }
+
+    public static void main(String[] args) {
+        new AesthethiccBot(args[0]);
+    }
+
+    private void handleIncomingQuery(InlineQueryEvent event) {
+        String input = event.getQuery().getQuery();
+
+        if (input.length() <= 1) {
+            return;
+        }
+
+        AnswerInlineQuery.AnswerInlineQueryBuilder builder = AnswerInlineQuery.builder();
+
+        for (int i = 0; i < transformers.size(); i++) {
+            TextTransformer transformer = transformers.get(i);
+            String output = transformer.transform(input);
+
+            builder.addResult(InlineResultArticle.builder()
+                    .id(String.valueOf(i))
+                    .title(transformer.getTitle())
+                    .description(output)
+                    .inputMessageContent(
+                            InputTextMessageContent.builder()
+                                    .messageText(output)
+                                    .parseMode(ParseMode.MARKDOWN)
+                                    .build()
+                    )
+                    .build());
+        }
+
+        bot.perform(builder.queryId(event.getQuery().getId())
+                .callback(() -> {})
+                .errorHandler((c) -> {
+                    System.out.println("Didn't work because " + c.getDescription());
+                })
+                .build());
+    }
+}
---
+++ src/main/java/com/mkotb/aesthethicc/transform/ClapperAesthethicc.java
@@ -0,0 +1,24 @@
+package com.mkotb.aesthethicc.transform;
+
+import com.mkotb.aesthethicc.TextTransformer;
+
+public class ClapperAesthethicc implements TextTransformer {
+    @Override
+    public String transform(String input) {
+        StringBuilder builder = new StringBuilder();
+        String splitter = " \uD83D\uDC4F ";
+
+        for (String str : input.split(" ")) {
+            builder.append(str);
+            builder.append(splitter);
+        }
+
+        builder.setLength(builder.length() - splitter.length());
+        return builder.toString();
+    }
+
+    @Override
+    public String getTitle() {
+        return "Clapper  \uD83D\uDC4F Aesthetic";
+    }
+}
