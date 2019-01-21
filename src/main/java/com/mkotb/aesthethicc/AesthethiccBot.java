0b8d417a602f9aed8eb68e841173f7c1a51f5e87
Mazen Kotb <me@mazenk.com>
2019-01-21 09:21:25 -0800

    Log queries in Elasticsearch

--- src/main/java/com/mkotb/aesthethicc/AesthethiccBot.java
+++ src/main/java/com/mkotb/aesthethicc/AesthethiccBot.java
@@ -3,6 +3,7 @@ package com.mkotb.aesthethicc;
 import com.jtelegram.api.TelegramBot;
 import com.jtelegram.api.TelegramBotRegistry;
 import com.jtelegram.api.events.inline.InlineQueryEvent;
+import com.jtelegram.api.inline.InlineQuery;
 import com.jtelegram.api.inline.input.InputTextMessageContent;
 import com.jtelegram.api.inline.result.InlineResultArticle;
 import com.jtelegram.api.requests.inline.AnswerInlineQuery;
@@ -10,13 +11,24 @@ import com.jtelegram.api.requests.message.framework.ParseMode;
 import com.jtelegram.api.update.PollingUpdateProvider;
 import com.mkotb.aesthethicc.transform.*;
 import lombok.Getter;
+import org.apache.http.HttpHost;
+import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestHighLevelClient;
 
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 @Getter
 public class AesthethiccBot {
+    private final ExecutorService service = Executors.newWorkStealingPool(3);
     private TelegramBot bot;
+    private RestHighLevelClient elasticClient;
     private List<TextTransformer> transformers = new ArrayList<>();
 
     private AesthethiccBot(String apiKey) {
@@ -25,6 +37,12 @@ public class AesthethiccBot {
         transformers.add(new ChillAesthethicc());
         transformers.add(new YellAesthethicc());
         transformers.add(new MockingAesthethicc());
+
+        elasticClient = new RestHighLevelClient(
+                RestClient.builder(
+                        new HttpHost("elasticsearch", 9200, "http")
+                )
+        );
 
         TelegramBotRegistry
                 .builder()
@@ -56,37 +74,59 @@ public class AesthethiccBot {
         new AesthethiccBot(System.getenv("TELEGRAM_KEY"));
     }
 
+    private void logQuery(InlineQuery query) {
+        Date time = new Date(System.currentTimeMillis());
+
+        service.execute(() -> {
+            try {
+                IndexRequest request = new IndexRequest("aesthethicc-queries", "doc");
+                request.source(
+                        "userId", query.getFrom().getId(),
+                        "query", query.getQuery(),
+                        "queryDate", time
+                );
+                elasticClient.index(request, RequestOptions.DEFAULT);
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        });
+    }
+
     private void handleIncomingQuery(InlineQueryEvent event) {
-        String input = event.getQuery().getQuery();
+        service.execute(() -> {
+            String input = event.getQuery().getQuery();
 
-        if (input.length() <= 1) {
-            return;
-        }
+            if (input.length() <= 1) {
+                return;
+            }
 
-        AnswerInlineQuery.AnswerInlineQueryBuilder builder = AnswerInlineQuery.builder();
+            AnswerInlineQuery.AnswerInlineQueryBuilder builder = AnswerInlineQuery.builder();
 
-        for (int i = 0; i < transformers.size(); i++) {
-            TextTransformer transformer = transformers.get(i);
-            String output = transformer.transform(input);
+            for (int i = 0; i < transformers.size(); i++) {
+                TextTransformer transformer = transformers.get(i);
+                String output = transformer.transform(input);
 
-            builder.addResult(InlineResultArticle.builder()
-                    .id(String.valueOf(i))
-                    .title(transformer.getTitle())
-                    .description(output)
-                    .inputMessageContent(
-                            InputTextMessageContent.builder()
-                                    .messageText(output)
-                                    .parseMode(ParseMode.MARKDOWN)
-                                    .build()
-                    )
+                builder.addResult(InlineResultArticle.builder()
+                        .id(String.valueOf(i))
+                        .title(transformer.getTitle())
+                        .description(output)
+                        .inputMessageContent(
+                                InputTextMessageContent.builder()
+                                        .messageText(output)
+                                        .parseMode(ParseMode.MARKDOWN)
+                                        .build()
+                        )
+                        .build());
+            }
+
+            logQuery(event.getQuery());
+
+            bot.perform(builder.queryId(event.getQuery().getId())
+                    .callback(() -> {})
+                    .errorHandler((c) -> {
+                        System.out.println("Didn't work because " + c.getDescription());
+                    })
                     .build());
-        }
-
-        bot.perform(builder.queryId(event.getQuery().getId())
-                .callback(() -> {})
-                .errorHandler((c) -> {
-                    System.out.println("Didn't work because " + c.getDescription());
-                })
-                .build());
+        });
     }
 }
